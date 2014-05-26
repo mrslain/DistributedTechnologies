cea6a9e828851b2e4394b820151dbb2d9e3c1edf
denis boginich <denis.boginich@skbkontur.ru>
2014-05-26 13:46:36 +0600

    * server для лабы про сложение 2х чисел

---
+++ ServerAndClient/src/Server.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server
+{
+    public Server(int port)
+    {
+        this.port = port;
+    }
+
+    public void run()
+    {
+        ServerSocket ss = null;
+
+        try
+        {
+            ss = new ServerSocket(port);
+
+            while(true) {
+                Socket client;
+                try {
+                    client = ss.accept();
+                }
+                catch (Exception e) {
+                    break;
+                }
+                if(client == null || client.isClosed())
+                    break;
+
+                DataInputStream input = null;
+                DataOutputStream output = null;
+
+                try
+                {
+                    input = new DataInputStream(client.getInputStream());
+                    output = new DataOutputStream(client.getOutputStream());
+                    output.writeInt(input.readInt() + input.readInt());
+                }
+                catch(Exception e)
+                {
+                    if(input != null)
+                        input.close();
+                    if(output != null)
+                        output.close();
+                    e.printStackTrace();
+                }
+
+                client.close();
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (ss != null)
+                    ss.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private int port;
+}
