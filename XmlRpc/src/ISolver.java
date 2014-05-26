10585cca72420567c1abafc91110c148651c0b3c
denis boginich <denis.boginich@skbkontur.ru>
2014-05-26 20:56:01 +0600

    * bug fix в вычислении корней квадратного уравнения

--- XmlRpc/src/ISolver.java
+++ XmlRpc/src/ISolver.java
@@ -1,3 +1,3 @@
 public interface ISolver {
-    public Solution solve(double a, double b, double c);
+    public Solution solve(double a, double b, double c) throws Exception;
 }
--- XmlRpc/src/Solver.java
+++ XmlRpc/src/Solver.java
@@ -1,5 +1,7 @@
 public class Solver implements ISolver {
-    public Solution solve(double a, double b, double c) {
+    public Solution solve(double a, double b, double c) throws Exception {
+//        throw new Exception("Server!!!");
+
         if(a == 0)
         {
             if(b == 0)
@@ -7,11 +9,11 @@ public class Solver implements ISolver {
             return new Solution(-c/b, -c/b);
         }
 
-        double D = b*b -4*a*c;
+        double D = b*b - 4*a*c;
         if(D<0)
             return Solution.DoesntExist();
-        double x1 = (-b + Math.sqrt(D))/2*a;
-        double x2 = (-b - Math.sqrt(D))/2*a;
+        double x1 = (-b + Math.sqrt(D))/(2*a);
+        double x2 = (-b - Math.sqrt(D))/(2*a);
         return new Solution(x1, x2);
     }
 }
