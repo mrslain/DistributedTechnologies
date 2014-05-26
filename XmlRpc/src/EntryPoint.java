08651f74cdcd4a69389a88f7811f3cdefb3c814e
denis boginich <denis.boginich@skbkontur.ru>
2014-05-26 20:40:52 +0600

    * реализовал solve у Solver(а)

--- XmlRpc/src/EntryPoint.java
+++ XmlRpc/src/EntryPoint.java
@@ -2,7 +2,7 @@ public class EntryPoint {
     public static void main(String[] args) {
         try {
             new ServerRunner().run(3146);
-            new RpcClient(3146).makeRequests(2);
+            new RpcClient(3146).makeRequests(0);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- XmlRpc/src/Solver.java
+++ XmlRpc/src/Solver.java
@@ -1,5 +1,17 @@
 public class Solver implements ISolver {
     public Solution solve(double a, double b, double c) {
-        return Solution.DoesntExist();
+        if(a == 0)
+        {
+            if(b == 0)
+                return Solution.DoesntExist();
+            return new Solution(-c/b, -c/b);
+        }
+
+        double D = b*b -4*a*c;
+        if(D<0)
+            return Solution.DoesntExist();
+        double x1 = (-b + Math.sqrt(D))/2*a;
+        double x2 = (-b - Math.sqrt(D))/2*a;
+        return new Solution(x1, x2);
     }
 }
