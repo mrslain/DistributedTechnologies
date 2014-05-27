0539fc3e2e81e94961d2c0af80900bc0bd6b18cf
denis boginich <denis.boginich@skbkontur.ru>
2014-05-27 20:32:58 +0600

    * пофиксил не проставленную Id(шку)

--- Queue/src/RequsetTaskData.java
+++ Queue/src/RequsetTaskData.java
@@ -1,7 +1,9 @@
 import java.io.Serializable;
+import java.util.UUID;
 
 public class RequsetTaskData implements Serializable, ITaskData {
     public RequsetTaskData(int a, int b) {
+        id = UUID.randomUUID().toString();
         this.a = a;
         this.b = b;
     }
